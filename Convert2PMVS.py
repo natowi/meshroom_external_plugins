96e0020e57aaeae9dd106c69c7fc2fe8fe21d103
natowi <natowi@users.noreply.github.com>
2020-02-15 10:19:07 +0100

    Update Convert2PMVS.py

--- Convert2PMVS.py
+++ Convert2PMVS.py
@@ -1,5 +1,13 @@
 # MR Sfm node -> convert2PMVS  (draft, WIP)
 
+# Optional parameters:
+#   --resolution arg (=1)              Divide image coefficient
+#   --nbCore arg (=8)                  Nb core
+#   --useVisData arg (=1)              Use visibility information.
+
+# Log parameters:
+#   -v [ --verboseLevel ] arg (=info)  verbosity level (fatal,  error, warning, info, debug, trace).
+
 __version__ = "1.1"
 
 from meshroom.core import desc
@@ -11,7 +19,7 @@ class Convert2PMVS(desc.CommandLineNode):
         desc.File(
                 name="input",
                 label="Input",
-                description="SFM input",
+                description="SFMData input",
                 value="",
                 uid=[0],
 		)
@@ -21,7 +29,7 @@ class Convert2PMVS(desc.CommandLineNode):
         desc.File(
             name="output",
             label="Output Folder",
-            description="Output in PMVS format",
+            description="Output path for keypoints",
             value=desc.Node.internalFolder,
             uid=[0],
             )
