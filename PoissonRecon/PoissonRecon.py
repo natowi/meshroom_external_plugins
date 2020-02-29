9fdcd29dbeb9864b5194c2c1ce462933590d4b11
natowi <natowi@users.noreply.github.com>
2020-02-29 12:39:53 +0100

    Update PoissonRecon.py

--- PoissonRecon/PoissonRecon.py
+++ PoissonRecon/PoissonRecon.py
@@ -1,1 +1,4 @@
 # https://github.com/mkazhdan/PoissonRecon
+# requires normals in pointcloud
+# unoriented point clouds to oriented point clouds
+# check out Parallel Globally Consistent Normal Orientation of Raw Unorganized Point Clouds
